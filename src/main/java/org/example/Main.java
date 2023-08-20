1ef86b8fd33db7b4ed6b1392579c46560658f4b2
LuzRdgz <luzrdgz15@gmail.com>
2023-08-19 23:52:48 -0700

    Version 1

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,53 @@
-package org.example;public class Main {
+package org.example;
+
+import com.opencsv.CSVWriter;
+
+import java.io.*;
+import java.util.Arrays;
+import java.util.List;
+
+
+public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        String archivoCSV = "C:\\Users\\LuzRd\\Desktop\\car_sales.csv";
+        BufferedReader lee = null;
+
+
+        // lectura de archivo CSV
+        try {
+            lee = new BufferedReader(new FileReader(archivoCSV));
+        }catch (FileNotFoundException e){
+            System.out.println("No se encuentra el nombre del archivo");
+        }
+
+        String lineaTxt = null;
+        String num;
+        Double valor;
+        Double vent[] = new Double[1000];
+
+        try{
+            while ((lineaTxt = lee.readLine()) != null){
+                num = lineaTxt.substring(lineaTxt.indexOf('$')).trim();
+                valor = Double.parseDouble(num.trim());
+                for (int i = 0; i< vent.length; i++){
+                    vent[i] = valor;
+                }
+            }
+        }catch(IOException e){
+            System.out.println("Error en la lectura del archivo");
+        }
+
+        Double sumP = null;
+        Double ventas[] = new Double[1000];
+
+
+        for (int i = 0; i< vent.length; i++){
+            sumP = sumP + vent[i];
+            ventas[i] = sumP;
+        }
+        for (int i = 0; i< vent.length; i++){
+            System.out.println(ventas[i]);
+        }
     }
 }
